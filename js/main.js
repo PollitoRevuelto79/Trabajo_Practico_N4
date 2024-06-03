d2551ec1bde3e56ac0ea33f7e413d5cfd7dd82c7
Iván Ramiro Villalba <ramiroivan79@hotmail.com>
2024-06-03 14:30:24 -0300

    Se termino la base

--- js/main.js
+++ js/main.js
@@ -1,19 +1,43 @@
-const url = 'https://rickandmortyapi.com/api/character'
+// function cargarDatos() {
+//     const url = 'https://rickandmortyapi.com/api/character';
+
+//     fetch(url)
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Error en la red - La respuesta no fue ok');
+//             }
+//             return response.json();
+//         })
+//         .then(data => {
+//             console.log('Datos recibidos:', data);
+//         })
+//         .catch(error => {
+//             console.error('Error capturado en .catch():', error);
+//         });
+// }
+
+// cargarDatos()
+
+
+const url = "https://rickandmortyapi.com/api/character"
 
 const procesarFetch = async (link) => {
     try {
         const respuesta = await fetch(link)
-        const info = await respuesta.json
+        const info = await respuesta.json()
         return info
     } catch (error) {
-        console.log("Hubo un error")
+        console.log("Hubo un error",error)
     }
 }
-link()
+procesarFetch(url).then(info => console.log(info));
 
 
+
+const elContainer = document.getElementById("container")
 procesarFetch(url)
     .then((info) => {
+        console.log(info)
         info.results.forEach((elemento) => {
             elContainer.innerHTML += `
             <div class="personaje">
